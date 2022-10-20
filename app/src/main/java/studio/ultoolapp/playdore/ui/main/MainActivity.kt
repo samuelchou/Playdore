ea820d3a59d9c3951803ae413404f4d06162ae33
Samuel Chou <samuelchou@hotmail.com.tw>
2022-10-20 18:00:56 +0800

    feat(main/view): 增加 View 標記，為 ViewModel 使用 Inject 做準備。

--- app/src/main/java/studio/ultoolapp/playdore/ui/main/MainActivity.kt
+++ app/src/main/java/studio/ultoolapp/playdore/ui/main/MainActivity.kt
@@ -2,8 +2,10 @@ package studio.ultoolapp.playdore.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import studio.ultoolapp.playdore.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/studio/ultoolapp/playdore/ui/main/MainFragment.kt
+++ app/src/main/java/studio/ultoolapp/playdore/ui/main/MainFragment.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import dagger.hilt.android.AndroidEntryPoint
 import studio.ultoolapp.playdore.databinding.FragmentMainBinding
 import studio.ultoolapp.playdore.viewModel.main.MainViewModel
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
     companion object {
         fun newInstance() = MainFragment()
