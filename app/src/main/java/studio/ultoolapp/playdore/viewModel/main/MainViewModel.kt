9e84502376d4c77ea58c7694e57c835e44d3fd46
Samuel Chou <samuelchou@hotmail.com.tw>
2022-10-20 17:47:28 +0800

    feat(main/view): ViewModel 改串接 Repository 作為資料來源。

--- app/src/main/java/studio/ultoolapp/playdore/viewModel/main/MainViewModel.kt
+++ app/src/main/java/studio/ultoolapp/playdore/viewModel/main/MainViewModel.kt
@@ -2,11 +2,17 @@ package studio.ultoolapp.playdore.viewModel.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import studio.ultoolapp.playdore.repository.WelcomeRepository
+import studio.ultoolapp.playdore.repository.WelcomeRepositoryImpl
 
 class MainViewModel : ViewModel() {
+    private val repository: WelcomeRepository = WelcomeRepositoryImpl()
+
     val message = MutableLiveData("Initialization")
 
     fun loadMessage() {
-        message.postValue("Hello World!")
+        repository.getWelcomeMessage().let {
+            message.postValue(it)
+        }
     }
 }
