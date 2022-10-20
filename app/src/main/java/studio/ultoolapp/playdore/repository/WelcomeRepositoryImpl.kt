7b1656da87fcb8a5a2693736959b80afb082b38c
Samuel Chou <samuelchou@hotmail.com.tw>
2022-10-20 17:59:40 +0800

    fix(main/model): 微調提供的 String.

--- app/src/main/java/studio/ultoolapp/playdore/repository/WelcomeRepositoryImpl.kt
+++ app/src/main/java/studio/ultoolapp/playdore/repository/WelcomeRepositoryImpl.kt
@@ -2,6 +2,6 @@ package studio.ultoolapp.playdore.repository
 
 class WelcomeRepositoryImpl : WelcomeRepository {
     override fun getWelcomeMessage(): String {
-        return "Hello User!"
+        return "Hello Repository Pattern!"
     }
 }
