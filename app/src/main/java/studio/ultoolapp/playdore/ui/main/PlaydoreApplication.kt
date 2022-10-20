19ddb191e2d781563baa890703e9247944e4d490
Samuel Chou <samuelchou@hotmail.com.tw>
2022-10-20 17:58:55 +0800

    feat(app): 改使用 Custom Application, 為未來使用 Hilt Inject 做準備。

---
+++ app/src/main/java/studio/ultoolapp/playdore/ui/main/PlaydoreApplication.kt
@@ -0,0 +1,8 @@
+package studio.ultoolapp.playdore.ui.main
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PlaydoreApplication : Application() {
+}
